c8d913b99e5c79262b4d7927c713d0b402e44aa1
Jasper Robison <jasper.robison22@gmail.com>
2021-07-30 13:18:22 +1000

    some DEPs

--- src/main/java/life/genny/bootxport/xlsimport/Optimization.java
+++ src/main/java/life/genny/bootxport/xlsimport/Optimization.java
@@ -34,10 +34,12 @@ public class Optimization {
     private static final String ATT_PREFIX= "ATT_";
     private static final String SER_PREFIX= "SER_";
     private static final String DFT_PREFIX= "DFT_";
+    private static final String DEP_PREFIX= "DEP_";
     private static final Map<String, String> defPrefixDataTypeMapping =
     Map.of(ATT_PREFIX,"DTT_BOOLEAN",
     SER_PREFIX, "DTT_JSON",
-    DFT_PREFIX, "DTT_TEXT");
+    DFT_PREFIX, "DTT_TEXT",
+    DEP_PREFIX, "DTT_TEXT");
 
     String debugStr = "Time profile";
 
