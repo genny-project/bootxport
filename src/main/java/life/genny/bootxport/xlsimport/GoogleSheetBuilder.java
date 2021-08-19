0bf15bc4f4bb3cb53d3340aaec08d9c9367a8672
Adam Crow <adamcrow63@gmail.com>
2021-08-19 21:36:04 +1000

    Added default false return if double does not get recognised

--- src/main/java/life/genny/bootxport/xlsimport/GoogleSheetBuilder.java
+++ src/main/java/life/genny/bootxport/xlsimport/GoogleSheetBuilder.java
@@ -85,7 +85,13 @@ public class GoogleSheetBuilder {
                         "[fFdD]?))" +
                         "[\\x00-\\x20]*");// Optional trailing "whitespace"
 
-        return Pattern.matches(fpRegex, doubleStr);
+      boolean result = false;
+        
+        try {
+			result = Pattern.matches(fpRegex, doubleStr);
+		} catch (Exception e) {
+			log.error("Error in isDouble ->"+doubleStr);
+		}
     }
 
     public static boolean getBooleanFromString(final String booleanString) {
